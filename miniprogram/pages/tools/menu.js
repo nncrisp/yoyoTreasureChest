5671637821b7309bdf1d6a0720ed17a96ddc5047
nncrisp <crispnn@qq.com>
2021-07-30 20:33:18 +0800

    增加了YOYO私房菜及部分界面。

--- miniprogram/pages/tools/menu.js
+++ miniprogram/pages/tools/menu.js
@@ -5,16 +5,29 @@ Page({
    * Page initial data
    */
   data: {
-
+    styles: [
+      {name:"荤菜", status: false},
+      {name:"素菜", status: false},
+      {name:"主食", status: false},
+      {name:"酒水", status: false}
+    ],
+    meals: [
+      {name:"早餐", status: false},
+      {name:"午餐", status: false},
+      {name:"晚餐", status: false},
+      {name:"节日餐", status: false}
+    ],
+    showResult: false, //是否展示菜谱名称
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
-
+  onLoad:function() {
+    wx.setNavigationBarTitle({
+      title: 'YOYO私房菜'
+    })
   },
-
   /**
    * Lifecycle function--Called when page is initially rendered
    */
