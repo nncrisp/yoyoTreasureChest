d6fb85d6f3e7d6d24a6cf54d1533d00469bcc6fe
nncrisp <crispnn@qq.com>
2021-10-03 20:28:05 +0800

    Added more buttons and more GUI logic for the buttons
    Buttons that have been added GUI logic: 数显, 开关。

--- miniprogram/pages/games/airconditioner.js
+++ miniprogram/pages/games/airconditioner.js
@@ -5,8 +5,9 @@ Page({
    * Page initial data
    */
   data: {
+    isOn: false, // isOn = true: 开机，isOn = false: 关机
     modes: [
-      {name:"循环风", img:"/images/air_recirculate.png", dft_tmp:25, dft_tmpf:77, max_tmp:25, min_tmp:25},
+      {name:"自动", img:"/images/air_recirculate.png", dft_tmp:25, dft_tmpf:77, max_tmp:25, min_tmp:25},
       {name:"制冷", img:"/images/air_cold.png", dft_tmp:25, dft_tmpf:77, max_tmp:33, min_tmp:9},
       {name:"制热", img:"/images/air_warm.png", dft_tmp:15.5, dft_tmpf:59.9, max_tmp:20, min_tmp:9},
       {name:"送风", img:"/images/air_wind.png", dft_tmp:25, dft_tmpf:77, max_tmp:33, min_tmp:19},
@@ -31,232 +32,279 @@ Page({
     natural_wind: "&emsp;&emsp;&emsp;&emsp;", //是否开启自然风：blank placeholders - 不开启，”自然风开“
     current_wind_dir: "", //显示风向：blank - 不显示, 不为空时则为鞥想的图片路径
     heat: "&emsp;&emsp;", //显示”辅热“或不显示，默认不显示
-    lock: "/images/unlock.png" //显示locked或unlocked的图片，默认为unlocked
+    lock: "/images/unlock.png", //显示locked或unlocked的图片，默认为unlocked
+    display: "/images/bulb.png" //显示“数显”图标，控制空调上是否显示内容。
+  },
+  //摁“开关”键切换开机和关机
+  onPower: function(){
+    if(this.data.lock == "/images/unlock.png"){
+      this.setData({
+        isOn: !this.data.isOn
+      })
+    }
   },
 
   //摁”模式“键切换各种模式
   onChangeMode: function(){
-    if(this.data.lock == "/images/unlock.png"){
-      this.setData({
-        current_mode_id: this.data.current_mode_id < 7 ? this.data.current_mode_id + 1 : 0,
-      })
-      var id = this.data.current_mode_id
-      this.setData({
-        current_tmp: this.data.modes[this.data.current_mode_id].dft_tmp,
-        current_tmpf: this.data.modes[this.data.current_mode_id].dft_tmpf
-      })
-      if (id == 0 || id == 4 || id == 5){
+    if(this.data.isOn==true){
+      if(this.data.lock == "/images/unlock.png"){
         this.setData({
-          canshow_sleep_wakeup: false,
-          isShown: 0,
-          sleep_wakeup_img_src: ""
+          current_mode_id: this.data.current_mode_id < 7 ? this.data.current_mode_id + 1 : 0,
         })
-      }
-      else this.setData({
-        canshow_sleep_wakeup: true
-      })
-      if (id != 2) {
+        var id = this.data.current_mode_id
         this.setData({
-          heat:"&emsp;&emsp;"
+          current_tmp: this.data.modes[this.data.current_mode_id].dft_tmp,
+          current_tmpf: this.data.modes[this.data.current_mode_id].dft_tmpf
         })
+        if (id == 0 || id == 4 || id == 5){
+          this.setData({
+            canshow_sleep_wakeup: false,
+            isShown: 0,
+            sleep_wakeup_img_src: ""
+          })
+        }
+        else this.setData({
+          canshow_sleep_wakeup: true
+        })
+        if (id != 2) {
+          this.setData({
+            heat:"&emsp;&emsp;"
+          })
+        }
       }
     }
+    else return
   },
 
   //摁“+1度，-1度，+0.1度，-0.1度”四个键时温度产生相应变化
   onChangeTmp: function(e){
-    if(this.data.lock == "/images/unlock.png"){
-      var id = parseInt(e.currentTarget.dataset.id)
-      var tmp = this.data.tmp_precision[id]
-      var new_tmp = 0
-      var max_tmp = 0
-      var min_tmp = 0
-      if (this.data.isC == true){//摄氏度
-        new_tmp = parseFloat((this.data.current_tmp + tmp).toFixed(1))
-        max_tmp = this.data.modes[this.data.current_mode_id].max_tmp
-        min_tmp = this.data.modes[this.data.current_mode_id].min_tmp
-      }
-      else {//华氏度
-        new_tmp = parseFloat((this.data.current_tmpf + tmp).toFixed(1))
-        max_tmp = this.data.modes[this.data.current_mode_id].max_tmp*1.8+32
-        min_tmp = this.data.modes[this.data.current_mode_id].min_tmp*1.8+32
-      }
-      if (new_tmp > max_tmp || new_tmp < min_tmp){
-          new_tmp = this.data.isC == true ? this.data.current_tmp : this.data.current_tmpf
-      }
-      //始终保持current_tmp和current_tmpf的正确换算关系
-      if (this.data.isC == true){
-        this.setData({
-          current_tmp: new_tmp,
-          current_tmpf: new_tmp * 1.8 + 32
-        })
-      }
-      else {
-        this.setData({
-          current_tmpf: new_tmp,
-          current_tmp: (new_tmp - 32)/1.8
-        })
+    if(this.data.isOn == true){
+      if(this.data.lock == "/images/unlock.png"){
+        var id = parseInt(e.currentTarget.dataset.id)
+        var tmp = this.data.tmp_precision[id]
+        var new_tmp = 0
+        var max_tmp = 0
+        var min_tmp = 0
+        if (this.data.isC == true){//摄氏度
+          new_tmp = parseFloat((this.data.current_tmp + tmp).toFixed(1))
+          max_tmp = this.data.modes[this.data.current_mode_id].max_tmp
+          min_tmp = this.data.modes[this.data.current_mode_id].min_tmp
+        }
+        else {//华氏度
+          new_tmp = parseFloat((this.data.current_tmpf + tmp).toFixed(1))
+          max_tmp = this.data.modes[this.data.current_mode_id].max_tmp*1.8+32
+          min_tmp = this.data.modes[this.data.current_mode_id].min_tmp*1.8+32
+        }
+        if (new_tmp > max_tmp || new_tmp < min_tmp){
+            new_tmp = this.data.isC == true ? this.data.current_tmp : this.data.current_tmpf
+        }
+        //始终保持current_tmp和current_tmpf的正确换算关系
+        if (this.data.isC == true){
+          this.setData({
+            current_tmp: new_tmp,
+            current_tmpf: new_tmp * 1.8 + 32
+          })
+        }
+        else {
+          this.setData({
+            current_tmpf: new_tmp,
+            current_tmp: (new_tmp - 32)/1.8
+          })
+        }
       }
     }
+    else return
   },
 
   //摁温度单位键C/F, 切换温度单位
   onChangeUnit: function(e){
-    if(this.data.lock == "/images/unlock.png"){
-      var unit = e.currentTarget.dataset.unit
-      if (unit == 'C') {
-        this.setData({
-          isC: true
-        })
-      }
-      else if (unit == 'F') {
-        this.setData({
-          isC: false
-        })
-      }
-      else console.log("单位错误：", unit)
-      }
+    if(this.data.isOn == true){
+      if(this.data.lock == "/images/unlock.png"){
+        var unit = e.currentTarget.dataset.unit
+        if (unit == 'C') {
+          this.setData({
+            isC: true
+          })
+        }
+        else if (unit == 'F') {
+          this.setData({
+            isC: false
+          })
+        }
+        else console.log("单位错误：", unit)
+        }
+    }
+    else return
   },
 
   //摁“睡眠”或“清醒”键，在某些特定模式下显示睡眠图标（月亮）或清醒图标（眼睛）
   onSleepOrWakeup: function(e){
-    if(this.data.lock == "/images/unlock.png"){
-      var i = e.currentTarget.dataset.i
-      if (this.data.canshow_sleep_wakeup == true){
-        if (i=="sleep"){
-          if (this.data.isShown == 0 || this.data.isShown == 2){
-            this.setData({
-              isShown: 1,
-              sleep_wakeup_img_src:"/images/sleep.png"
-            })
+    if(this.data.isOn == true){
+      if(this.data.lock == "/images/unlock.png"){
+        var i = e.currentTarget.dataset.i
+        if (this.data.canshow_sleep_wakeup == true){
+          if (i=="sleep"){
+            if (this.data.isShown == 0 || this.data.isShown == 2){
+              this.setData({
+                isShown: 1,
+                sleep_wakeup_img_src:"/images/sleep.png"
+              })
+            }
+            else if (this.data.isShown == 1){
+              this.setData({
+                isShown: 0,
+                sleep_wakeup_img_src: ""
+              })
+            }
+            else {
+              console.log("isShown赋值错误：",this.data.isShown)
+            }
           }
-          else if (this.data.isShown == 1){
-            this.setData({
-              isShown: 0,
-              sleep_wakeup_img_src: ""
-            })
-          }
-          else {
-            console.log("isShown赋值错误：",this.data.isShown)
+          else if (i=="wakeup"){
+            if (this.data.isShown == 0 || this.data.isShown == 1){
+              this.setData({
+                isShown: 2,
+                sleep_wakeup_img_src: "/images/wakeup.png"
+              })
+            }
+            else if (this.data.isShown == 2){
+              this.setData({
+                isShown: 0,
+                sleep_wakeup_img_src: ""
+              })
+            }
+            else {
+              console.log("isShown赋值错误：",this.data.isShown)
+            }
           }
         }
-        else if (i=="wakeup"){
-          if (this.data.isShown == 0 || this.data.isShown == 1){
-            this.setData({
-              isShown: 2,
-              sleep_wakeup_img_src: "/images/wakeup.png"
-            })
-          }
-          else if (this.data.isShown == 2){
-            this.setData({
-              isShown: 0,
-              sleep_wakeup_img_src: ""
-            })
-          }
-          else {
-            console.log("isShown赋值错误：",this.data.isShown)
-          }
-        }
-      }
-      else this.setData({
-        sleep_wakeup_img_src: ""
-      }) 
-    } 
+        else this.setData({
+          sleep_wakeup_img_src: ""
+        }) 
+      } 
+    }
+    else return
   },
 
   onChangeWind: function(){
-    if(this.data.lock == "/images/unlock.png"){
-      if(this.data.current_wind_num >=0 && this.data.current_wind_num < 33){
-        this.setData({
-          isAutoWind: false,
-          current_wind_num: this.data.current_wind_num + 1
-        })
-      }
-      else if(this.data.current_wind_num == 33){
-        this.setData({
-          isAutoWind: true,
-          current_wind_num: 0
-        })
-      }
-      else console.log("错误的风速：",this.data.current_wind)
-      if(this.data.current_wind_num >= 10 && this.data.current_wind_num<=33){
-        this.setData({
-          current_wind:this.data.current_wind_num.toString()
-        })
-      }
-      else if (this.data.current_wind_num > 0 && this.data.current_wind_num <=9){
-        var prefix = "&nbsp;"
-        this.setData({
-          current_wind: prefix.concat(this.data.current_wind_num.toString())
-        })
+    if(this.data.isOn==true){
+      if(this.data.lock == "/images/unlock.png"){
+        if(this.data.current_wind_num >=0 && this.data.current_wind_num < 33){
+          this.setData({
+            isAutoWind: false,
+            current_wind_num: this.data.current_wind_num + 1
+          })
         }
-        else this.setData({
-          current_wind: "&nbsp;&nbsp;"
-        })
+        else if(this.data.current_wind_num == 33){
+          this.setData({
+            isAutoWind: true,
+            current_wind_num: 0
+          })
+        }
+        else console.log("错误的风速：",this.data.current_wind)
+        if(this.data.current_wind_num >= 10 && this.data.current_wind_num<=33){
+          this.setData({
+            current_wind:this.data.current_wind_num.toString()
+          })
+        }
+        else if (this.data.current_wind_num > 0 && this.data.current_wind_num <=9){
+          var prefix = "&nbsp;"
+          this.setData({
+            current_wind: prefix.concat(this.data.current_wind_num.toString())
+          })
+          }
+          else this.setData({
+            current_wind: "&nbsp;&nbsp;"
+          })
+      }
     }
+    else return
 },
 
   onSterilize: function(){
-    if(this.data.lock == "/images/unlock.png"){
-      this.setData({
-        sterilize: this.data.sterilize == "&emsp;&emsp;"?"除菌":"&emsp;&emsp;"
-      })
+    if(this.data.isOn == true){
+      if(this.data.lock == "/images/unlock.png"){
+        this.setData({
+          sterilize: this.data.sterilize == "&emsp;&emsp;"?"除菌":"&emsp;&emsp;"
+        })
+      }
     }
+    else return
   },
 
   onSaveEnergy: function(){
-    if(this.data.lock == "/images/unlock.png"){
-      this.setData({
-        save_energy: this.data.save_energy == "&emsp;&emsp;"?"省电":"&emsp;&emsp;"
-      })
+    if(this.data.isOn == true){
+      if(this.data.lock == "/images/unlock.png"){
+        this.setData({
+          save_energy: this.data.save_energy == "&emsp;&emsp;"?"省电":"&emsp;&emsp;"
+        })
+      }
     }
   },
 
   onNaturalWind: function(){
-    if(this.data.lock == "/images/unlock.png"){
-      this.setData({
-        natural_wind: this.data.natural_wind == "&emsp;&emsp;&emsp;&emsp;"?"自然风开":"&emsp;&emsp;&emsp;&emsp;"
-      })
+    if(this.data.isOn == true){
+      if(this.data.lock == "/images/unlock.png"){
+        this.setData({
+          natural_wind: this.data.natural_wind == "&emsp;&emsp;&emsp;&emsp;"?"自然风开":"&emsp;&emsp;&emsp;&emsp;"
+        })
+      }
     }
+    else return
   },
 
   onWindDirection: function(e){
-    if(this.data.lock == "/images/unlock.png"){
-      var dir = e.currentTarget.dataset.dir
-      if (this.data.current_wind_dir == ""){
-        this.setData({
-          current_wind_dir: dir
-        })
-      }
-      else if (this.data.current_wind_dir == dir){
-        this.setData({
-          current_wind_dir: ""
-        })
-      }
-      else {
-        this.setData({
-          current_wind_dir: dir
-        })
-      }
-    } 
+    if(this.data.isOn == true){
+      if(this.data.lock == "/images/unlock.png"){
+        var dir = e.currentTarget.dataset.dir
+        if (this.data.current_wind_dir == ""){
+          this.setData({
+            current_wind_dir: dir
+          })
+        }
+        else if (this.data.current_wind_dir == dir){
+          this.setData({
+            current_wind_dir: ""
+          })
+        }
+        else {
+          this.setData({
+            current_wind_dir: dir
+          })
+        }
+      } 
+    }
+    else return
   },
   //辅热
   onHeat: function(){
-    if(this.data.lock == "/images/unlock.png" && this.data.current_mode_id == 2){
-      if (this.data.current_mode_id == 2){
-        this.setData({
-          heat: this.data.heat=="&emsp;&emsp;"?"辅热":"&emsp;&emsp;"
-        })
+    if(this.data.isOn == true){
+      if(this.data.lock == "/images/unlock.png" && this.data.current_mode_id == 2){
+        if (this.data.current_mode_id == 2){
+          this.setData({
+            heat: this.data.heat=="&emsp;&emsp;"?"辅热":"&emsp;&emsp;"
+          })
+        }
       }
     }
+    else return
   },
   //锁定
   onLock: function(){
-    this.setData({
-      lock: this.data.lock == "/images/unlock.png"?"/images/lock.png":"/images/unlock.png"
-    })
+    if(this.data.isOn == true){
+      this.setData({
+        lock: this.data.lock == "/images/unlock.png"?"/images/lock.png":"/images/unlock.png"
+      })
+    }
   },
-
+  //数显：
+  onDisplay: function(){
+    if(this.data.lock == "/images/unlock.png"){
+      this.setData({
+        display: this.data.display == "/images/bulb.png"?"":"/images/bulb.png"
+      })
+    }
+  },
   /**
    * Lifecycle function--Called when page load
    */
